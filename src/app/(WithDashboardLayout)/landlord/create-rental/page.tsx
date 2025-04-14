e4b24c0cd3cd082e09df0178d495e01cb788ddb5
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2025-04-14 13:22:21 +0600

    Added Create Rental house

---
+++ src/app/(WithDashboardLayout)/landlord/create-rental/page.tsx
@@ -0,0 +1,14 @@
+import CreateRentalForm from "@/components/modules/rentalHouse/rentalForm/CreateRentalForm";
+import React from "react";
+
+const CreateRental = () => {
+  return (
+    <div>
+      <div className="container mx-auto px-3 my-10">
+        <CreateRentalForm />
+      </div>
+    </div>
+  );
+};
+
+export default CreateRental;
