52d0fd0430ce63d9c94074e812d49aee4d4393c0
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2025-04-15 21:09:59 +0600

    Updated admin dashboard table

--- src/services/Users/index.ts
+++ src/services/Users/index.ts
@@ -1,4 +1,5 @@
 "use server";
+import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 
 export const getAllUsers = async () => {
@@ -10,6 +11,9 @@ export const getAllUsers = async () => {
             headers: {
                 Authorization: token,
             },
+            next: {
+                tags: ["USER"]
+            }
         });
 
         if (!res.ok) {
@@ -19,5 +23,51 @@ export const getAllUsers = async () => {
         return res.json();
     } catch (error: any) {
         return new Error(error.message || "Something went wrong");
+    }
+};
+
+export const deleteUser = async (userId: string) => {
+    try {
+        const token = (await cookies()).get("accessToken")?.value || "";
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/admin/users/${userId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: token,
+            },
+        });
+
+        if (!res.ok) {
+            throw new Error(`Failed to delete user: ${res.status}`);
+        }
+        revalidateTag("USER")
+
+        return { success: true };
+    } catch (error: any) {
+        return { success: false, message: error.message || "Something went wrong" };
+    }
+};
+
+
+export const updateUserRole = async (userId: string, newRole: string) => {
+    try {
+        const token = (await cookies()).get("accessToken")?.value || "";
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/admin/users/${userId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+            },
+            body: JSON.stringify({ role: newRole }),
+        });
+
+        if (!res.ok) {
+            throw new Error(`Failed to update role: ${res.status}`);
+        }
+
+        return { success: true };
+    } catch (error: any) {
+        return { success: false, message: error.message || "Something went wrong" };
     }
 };
