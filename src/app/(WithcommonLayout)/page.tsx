1aa33b48dd4d0e2db125ea0d3ec047ae07f6c041
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-04-17 16:37:07 +0600

    testimonial and tips section made for home page

--- src/app/(WithcommonLayout)/page.tsx
+++ src/app/(WithcommonLayout)/page.tsx
@@ -2,6 +2,8 @@
 
 import Banner from "@/components/modules/banner/Banner";
 import Cards from "@/components/modules/rentalHouse/rentalHouseCard/Cards";
+import RentingTips from "@/components/modules/Testimonial/RentingTips";
+import Testimonial from "@/components/modules/Testimonial/Testimonial";
 import { useUser } from "@/context/UserContext";
 import React from "react";
 
@@ -14,6 +16,8 @@ const HomePage = () => {
       <Banner />
       <div className="container mx-auto px-3 my-10 lg:my-20">
         <Cards />
+        <Testimonial/>
+        <RentingTips/>
       </div>
     </div>
   );
